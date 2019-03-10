4333387725da1745dd7a27d76b43dfe996de5ea8
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-10 22:56:59 +0100

    improve SelectField

--- src/SelectField.tsx
+++ src/SelectField.tsx
@@ -1,22 +1,23 @@
 import { Select } from "antd";
 import { Field, FormikProps } from "formik";
 import * as React from "react";
+import { SelectProps } from "antd/lib/select";
 
-export const SelectField = ({
-  name,
-  children
-}: {
-  name: string;
-  children: any;
-}) => (
-  <Field name={name}>
-    {({ field, form }: { field: any; form: FormikProps<any> }) => (
-      <Select
-        onChange={value => form.setFieldValue(name, value)}
-        value={field.value}
-      >
-        {children}
-      </Select>
-    )}
-  </Field>
-);
+type Props = { name: string; children: any } & SelectProps<any>;
+
+export const SelectField = (props: Props) => {
+  const { name } = props;
+  return (
+    <Field name={name}>
+      {({ field, form }: { field: any; form: FormikProps<any> }) => (
+        <Select
+          {...props}
+          onChange={value => form.setFieldValue(name, value)}
+          value={field.value}
+        >
+          {props.children}
+        </Select>
+      )}
+    </Field>
+  );
+};
