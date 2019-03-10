2dadd4476c94640ca790c3138568ea2af77e363f
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-10 20:02:27 +0100

    minor

--- src/FormikDebugField.tsx
+++ src/FormikDebugField.tsx
@@ -4,7 +4,9 @@ import { isDevelopmentMode } from "./utils";
 
 export const FormikDebug = () =>
   isDevelopmentMode() ? (
-    <Field>{({ form }: any) => <DebugView {...form} />}</Field>
+    <div style={{ padding: 15 }}>
+      <Field>{({ form }: any) => <DebugView {...form} />}</Field>
+    </div>
   ) : null;
 
 export const DebugView = (data: any) =>
