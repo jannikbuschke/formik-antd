bf77afc84148d4c82b3af3a7d9dabbbe436be224
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-16 13:31:17 +0100

    remove Form.Item from TextAreaField and fix api

--- src/TextAreaField.tsx
+++ src/TextAreaField.tsx
@@ -1,27 +1,19 @@
-import { Form, Input } from "antd";
-import { FormItemProps } from "antd/lib/form";
+import { Input } from "antd";
 import { TextAreaProps } from "antd/lib/input";
-import { Field, FormikProps } from "formik";
+import { Field, FieldProps } from "formik";
 import * as React from "react";
 
-export const TextAreaField = (props: TextAreaProps & FormItemProps) => (
-  <Field {...props}>
-    {({ field, form }: { field: any; form: FormikProps<any> }) => {
-      const hasError = form.errors && form.errors[field.name.toLowerCase()];
-
-      if (hasError) {
-        return (
-          <Form.Item
-            label={props.label}
-            validateStatus="error"
-            hasFeedback={false}
-            help={form.errors[field.name.toLowerCase()]}
-          >
-            <Input.TextArea {...props} {...field} />
-          </Form.Item>
-        );
-      }
-      return <Input.TextArea {...props} {...field} />;
+export const TextAreaField = (props: TextAreaProps & { name: string }) => (
+  <Field name={props.name}>
+    {({ field }: FieldProps) => {
+      return (
+        <Input.TextArea
+          value={field.value}
+          onChange={field.onChange}
+          onBlur={field.onBlur}
+          {...props}
+        />
+      );
     }}
   </Field>
 );
