ddf31aa5cfd65221243c10d74f6648320d899b31
Jannik Buschke <jannik.buschke@gmail.com>
2019-03-16 14:17:11 +0100

    fix SwitchField api

--- src/SwitchField.tsx
+++ src/SwitchField.tsx
@@ -1,13 +1,16 @@
-import { Switch } from "antd";
-import { Field, FormikProps } from "formik";
+import { Switch, Checkbox } from "antd";
+import { Field, FormikProps, FieldProps } from "formik";
 import * as React from "react";
+import { SwitchProps } from "antd/lib/switch";
+import { CheckboxProps } from "antd/lib/checkbox/Checkbox";
 
-export const SwitchField = (props: any) => (
-  <Field {...props}>
+export const SwitchField = (props: { name: string } & SwitchProps) => (
+  <Field name={props.name}>
     {({ field, form }: { field: any; form: FormikProps<any> }) => (
       <Switch
         checked={field.value}
         onChange={e => form.setFieldValue(props.name, e)}
+        {...props}
       />
     )}
   </Field>
