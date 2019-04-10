7c0c0751a31a372e690ec48620ec2b1789ba8218
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-10 21:39:24 +0200

    format

--- src/EnumStringEditor.tsx
+++ src/EnumStringEditor.tsx
@@ -4,19 +4,28 @@ import * as React from "react";
 import { RadioGroupProps } from "antd/lib/radio/interface";
 import { FormikFieldProps, IDataSourceObject } from "./FieldProps";
 
-export const EnumStringEditor = (
-  { name, validate, dataSource, children, ...restProps }: FormikFieldProps & RadioGroupProps & { dataSource?: Array<IDataSourceObject>, children?: React.ReactNode }
-) => (
+export const EnumStringEditor = ({
+  name,
+  validate,
+  dataSource,
+  children,
+  ...restProps
+}: FormikFieldProps &
+  RadioGroupProps & {
+    dataSource?: Array<IDataSourceObject>;
+    children?: React.ReactNode;
+  }) => (
   <Field name={name} validate={validate}>
     {({ field, form }: FieldProps) => (
-        <Radio.Group {...restProps}>
-          {Array.isArray( dataSource ) && dataSource.map(({ value, label }: IDataSourceObject ) => (
+      <Radio.Group {...restProps}>
+        {Array.isArray(dataSource) &&
+          dataSource.map(({ value, label }: IDataSourceObject) => (
             <Radio key={`radio-${value}`} value={value}>
               {label}
             </Radio>
           ))}
-          {children}
-        </Radio.Group>
+        {children}
+      </Radio.Group>
     )}
   </Field>
 );
