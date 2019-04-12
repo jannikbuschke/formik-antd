e48a7dfe814fc98b8a1e6149a43fdd1d83dcf604
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-13 01:24:03 +0200

    rename Select

--- src/SelectField.tsx
+++ src/SelectField.tsx
@@ -1,27 +1,34 @@
-import { Select } from "antd";
+import { Select as $Select } from "antd";
 import { Field, FieldProps } from "formik";
 import * as React from "react";
 import { SelectProps, OptionProps } from "antd/lib/select";
 import { FormikFieldProps } from "./FieldProps";
 
-export const SelectField = (
-  { name, validate, children, ...restProps }: FormikFieldProps & SelectProps<any> & { children: React.ReactNode }
-) => {
+export const Select = ({
+  name,
+  validate,
+  children,
+  ...restProps
+}: FormikFieldProps & SelectProps<any> & { children: React.ReactNode }) => {
   return (
     <Field name={name} validate={validate}>
       {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
-        <Select
+        <$Select
           onChange={v => setFieldValue(name, v)}
           value={value}
           {...restProps}
         >
           {children}
-        </Select>
+        </$Select>
       )}
     </Field>
   );
 };
 
-type Option = OptionProps & { label: ( React.ReactNode | string | number ) };
-SelectField.renderOptions = (options: Option[]) =>
-  options.map(({ label,...restProps }, index) => <Select.Option key={`select-option-${index}`} {...restProps} >{label}</Select.Option>);
+type Option = OptionProps & { label: React.ReactNode | string | number };
+Select.renderOptions = (options: Option[]) =>
+  options.map(({ label, ...restProps }, index) => (
+    <$Select.Option key={`select-option-${index}`} {...restProps}>
+      {label}
+    </$Select.Option>
+  ));
