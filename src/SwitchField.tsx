905c98856180109347a0ec12d55cb29af41e4681
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-13 01:22:11 +0200

    rename Switch

--- src/SwitchField.tsx
+++ src/SwitchField.tsx
@@ -1,15 +1,17 @@
-import { Switch } from "antd";
+import { Switch as $Switch } from "antd";
 import { Field, FieldProps } from "formik";
 import * as React from "react";
 import { SwitchProps } from "antd/lib/switch";
 import { FormikFieldProps } from "./FieldProps";
 
-export const SwitchField = (
-  { name, validate, ...restProps }: FormikFieldProps & SwitchProps
-) => (
+export const Switch = ({
+  name,
+  validate,
+  ...restProps
+}: FormikFieldProps & SwitchProps) => (
   <Field name={name} validate={validate}>
     {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
-      <Switch
+      <$Switch
         checked={value}
         onChange={v => setFieldValue(name, v)}
         {...restProps}
