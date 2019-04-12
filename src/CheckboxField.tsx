04ad1e1df9cb9b2aff245a03058d422c36633c87
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-13 01:20:56 +0200

    rename Checkbox and make Group a function property

--- src/CheckboxField.tsx
+++ src/CheckboxField.tsx
@@ -1,15 +1,39 @@
-import { Checkbox } from "antd";
+import { Checkbox as $Checkbox } from "antd";
 import { Field, FieldProps } from "formik";
 import * as React from "react";
 import { CheckboxProps } from "antd/lib/checkbox/Checkbox";
 import { FormikFieldProps } from "./FieldProps";
+import { CheckboxGroupProps } from "antd/lib/checkbox/Group";
 
-export const CheckboxField = (
-  { name, validate, ...restProps }: FormikFieldProps & CheckboxProps
-) => (
+export const Checkbox = ({
+  name,
+  validate,
+  ...restProps
+}: FormikFieldProps & CheckboxProps) => (
   <Field name={name} validate={validate}>
     {({ field: { value, onChange } }: FieldProps) => (
-      <Checkbox name={name} checked={value} onChange={onChange} {...restProps} />
+      <$Checkbox
+        name={name}
+        checked={value}
+        onChange={onChange}
+        {...restProps}
+      />
+    )}
+  </Field>
+);
+
+Checkbox.Group = ({
+  name,
+  validate,
+  ...restProps
+}: FormikFieldProps & CheckboxGroupProps) => (
+  <Field name={name} validate={validate}>
+    {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
+      <$Checkbox.Group
+        value={value}
+        onChange={v => setFieldValue(name, v)}
+        {...restProps}
+      />
     )}
   </Field>
 );
