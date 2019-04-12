00d33196e1be19d1a1f7a1bd4d8b0593f842b776
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-13 01:23:35 +0200

    rename DatePicker

--- src/DatePickerField.tsx
+++ src/DatePickerField.tsx
@@ -1,16 +1,18 @@
-import { DatePicker } from "antd";
+import { DatePicker as $DatePicker } from "antd";
 import { Field, FieldProps } from "formik";
 import * as React from "react";
 import { DatePickerProps } from "antd/lib/date-picker/interface";
 import moment from "moment";
 import { FormikFieldProps } from "./FieldProps";
 
-export const DatePickerField = (
-  { name, validate, ...restProps }: FormikFieldProps & DatePickerProps
-) => (
+export const DatePicker = ({
+  name,
+  validate,
+  ...restProps
+}: FormikFieldProps & DatePickerProps) => (
   <Field name={name} validate={validate}>
     {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
-      <DatePicker
+      <$DatePicker
         value={value ? moment(value) : undefined}
         onChange={date => setFieldValue(name, date ? date.toISOString() : null)}
         {...restProps}
