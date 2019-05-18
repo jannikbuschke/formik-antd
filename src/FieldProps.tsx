4a3ccf2625ca54a428ba7ad96e969e36764a4ba0
Jannik Buschke <jannik.buschke@gmail.com>
2019-05-18 12:01:41 +0200

    remove IDataSourceObject

--- src/FieldProps.tsx
+++ src/FieldProps.tsx
@@ -2,8 +2,3 @@ export interface FormikFieldProps {
   name: string;
   validate?: (value: any) => undefined | string | Promise<any>;
 }
-
-export interface IDataSourceObject {
-  value: ( string | number );
-  label: ( string | React.ReactNode );
-}
