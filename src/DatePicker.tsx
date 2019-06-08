4b1903cb83686edb3688fe670162f699fb49d75b
Jannik Buschke <jannik.buschke@gmail.com>
2019-06-08 20:01:01 +0200

    add DatePicker.MonthPicker

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { DatePicker as $DatePicker } from "antd";
 import { Field, FieldProps } from "formik";
-import { DatePickerProps as $DatePickerProps } from "antd/lib/date-picker/interface";
+import { DatePickerProps as $DatePickerProps, MonthPickerProps as $MonthPickerProps } from "antd/lib/date-picker/interface";
 import moment from "moment";
 import { FormikFieldProps } from "./FieldProps";
+
+const { MonthPicker: $MonthPicker, RangePicker: $RangePicker, WeekPicker: $WeekPicker } = $DatePicker;
+
 
 export type DatePickerProps = FormikFieldProps & $DatePickerProps;
 
@@ -11,6 +14,20 @@ export const DatePicker = ({ name, validate, ...restProps }: DatePickerProps) =>
   <Field name={name} validate={validate}>
     {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
       <$DatePicker
+        value={value ? moment(value) : undefined}
+        onChange={date => setFieldValue(name, date ? date.toISOString() : null)}
+        {...restProps}
+      />
+    )}
+  </Field>
+);
+
+export type MonthPickerProps = FormikFieldProps & $MonthPickerProps;
+
+DatePicker.MonthPicker = ({ name, validate, ...restProps }: MonthPickerProps) => (
+  <Field name={name} validate={validate}>
+    {({ field: { value }, form: { setFieldValue } }: FieldProps) => (
+      <$MonthPicker
         value={value ? moment(value) : undefined}
         onChange={date => setFieldValue(name, date ? date.toISOString() : null)}
         {...restProps}
