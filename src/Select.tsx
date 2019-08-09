2e735dc37f6ab7a43bce11c531e07917b429ae92
Jannik Buschke <jannik.buschke@gmail.com>
2019-08-09 10:47:12 +0200

    fix onChange not invoked for Select

--- src/Select.tsx
+++ src/Select.tsx
@@ -6,12 +6,15 @@ import { FormikFieldProps } from "./FieldProps";
 
 export type SelectProps = FormikFieldProps & $SelectProps<any> & { children: React.ReactNode };
 
-export const Select = ({ name, validate, children, ...restProps }: SelectProps) => {
+export const Select = ({ name, validate, children, onChange, ...restProps }: SelectProps) => {
   return (
     <Field name={name} validate={validate}>
       {({ field: { value }, form: { setFieldValue, setFieldTouched } }: FieldProps) => (
         <$Select
-          onChange={v => setFieldValue(name, v)}
+          onChange={(value, option) => {
+            setFieldValue(name, value)
+            onChange && onChange(value, option)
+          }}
           onBlur={() => setFieldTouched(name)}
           value={value}
           {...restProps}
