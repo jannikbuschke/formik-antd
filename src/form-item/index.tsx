6de77fab36e99509e67eb1ebcc5040e72e3db607
claneo <claneo@outlook.com>
2019-08-26 11:19:04 +0800

    automatically set label and required for FormItem if using yup

--- src/form-item/index.tsx
+++ src/form-item/index.tsx
@@ -1,29 +1,64 @@
 import * as React from "react";
 import get from "lodash.get";
-import { Field, FieldProps } from "formik";
+import { Field, FieldProps, connect } from "formik";
 import { Form } from "antd";
 import { FormItemProps as $FormItemProps } from "antd/lib/form/FormItem";
+import isSchema from "yup/lib/util/isSchema";
+import reach from "yup/lib/util/reach";
 
-export type FormItemProps = { showValidateSuccess?: boolean; children: React.ReactNode } & { name: string } & $FormItemProps;
+export type FormItemProps = {
+  showValidateSuccess?: boolean;
+  children: React.ReactNode;
+} & { name: string } & $FormItemProps;
 
-export const FormItem = ({ name, showValidateSuccess, children, ...restProps }: FormItemProps) => (
-  <Field name={name}>
-    {({ form: { errors = {}, touched = {} } }: FieldProps) => {
-      const error = get(errors, name, undefined);
-      const isTouched = get(touched, name, false) as boolean;
-      const hasError = error !== undefined && isTouched;
-      const isValid = !error && isTouched;
-      return (
-        <Form.Item
-          validateStatus={hasError ? "error" : (isValid && showValidateSuccess) ? "success" : undefined}
-          hasFeedback={isValid}
-          help={(hasError && <li>{error}</li>) || (isValid && "")}
-          {...restProps}
-        >
-          {React.isValidElement(children) ? React.cloneElement(children, { name }) : children}
-        </Form.Item>
-      );
-    }}
-  </Field >);
+export const FormItem = connect<FormItemProps>(
+  ({
+    name,
+    showValidateSuccess,
+    children,
+    formik,
+    required,
+    label,
+    ...restProps
+  }) => (
+    <Field name={name}>
+      {({ form: { errors = {}, touched = {} } }: FieldProps) => {
+        const error = get(errors, name, undefined);
+        const isTouched = get(touched, name, false) as boolean;
+        const hasError = error !== undefined && isTouched;
+        const isValid = !error && isTouched;
+        if (isSchema(formik.validationSchema)) {
+          label =
+            label || reach(formik.validationSchema, name).describe().label;
+          required =
+            required ||
+            reach(formik.validationSchema, name).tests.some(
+              (item: any) => item.OPTIONS.name === "required"
+            );
+        }
+        return (
+          <Form.Item
+            validateStatus={
+              hasError
+                ? "error"
+                : isValid && showValidateSuccess
+                ? "success"
+                : undefined
+            }
+            label={label}
+            required={required}
+            hasFeedback={isValid}
+            help={(hasError && <li>{error}</li>) || (isValid && "")}
+            {...restProps}
+          >
+            {React.isValidElement(children)
+              ? React.cloneElement<any>(children, { name })
+              : children}
+          </Form.Item>
+        );
+      }}
+    </Field>
+  )
+);
 
-export default FormItem
+export default FormItem;
---
+++ src/form-item/yup.d.ts
@@ -0,0 +1,2 @@
+declare module "yup/lib/util/isSchema";
+declare module "yup/lib/util/reach";
