d808d2f47e79a3862419a3f2b748bfe63609d42b
Khartir <thomas.gnandt@mayflower.de>
2019-10-03 05:55:54 +0200

    add tests

---
+++ src/form-item/index.test.tsx
@@ -0,0 +1,63 @@
+import "@testing-library/jest-dom/extend-expect";
+import React, { ReactNode } from "react";
+import { Formik } from "formik";
+import { render, fireEvent, waitForDomChange } from "@testing-library/react";
+import FormItem from ".";
+import Input from "../input";
+import Form from "../form/form";
+import Select from "../select";
+import SubmitButton from "../submit-button";
+const { Option } = Select;
+
+const Test = ({
+  children,
+  validate
+}: {
+  children: ReactNode;
+  validate?: () => object;
+}) => {
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}} validate={validate}>
+      <Form>
+        <FormItem name="test">{children}</FormItem>
+        <SubmitButton data-testid="submit" />
+      </Form>
+    </Formik>
+  );
+};
+
+test("displays validation result", async () => {
+  const validate = () => ({ test: "error" });
+  const { getByTestId, queryByText } = render(
+    <Test validate={validate}>
+      <Input name="test" data-testid="input" />
+    </Test>
+  );
+  expect(queryByText("error")).not.toBeInTheDocument();
+  fireEvent.change(getByTestId("input"), {
+    target: { name: "test", value: "test" }
+  });
+  fireEvent.click(getByTestId("submit"));
+  await waitForDomChange();
+  expect(queryByText("error")).toBeInTheDocument();
+});
+
+test("handles changes on multiselect without prop-types error", async () => {
+  const { getByTestId, queryByText, getByText } = render(
+    <Test>
+      <Select name="test" data-testid="input" mode="multiple">
+        <Option value="1">1</Option>
+        <Option value="2">2</Option>
+      </Select>
+    </Test>
+  );
+  expect(queryByText("error")).not.toBeInTheDocument();
+  fireEvent.click(getByTestId("input"));
+  fireEvent.click(getByText("1"));
+  console.error = jest.fn();
+  fireEvent.click(getByTestId("submit"));
+  await waitForDomChange();
+  expect(console.error).not.toHaveBeenCalled();
+  //@ts-ignore
+  console.error.mockRestore();
+});
