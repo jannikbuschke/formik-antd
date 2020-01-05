d2e68a77824fbfdac487850ca88d29ffd947c808
Jannik Buschke <jannik.buschke@gmail.com>
2020-01-05 20:47:31 +0100

    fix some tests

--- src/form-item/index.test.tsx
+++ src/form-item/index.test.tsx
@@ -9,6 +9,21 @@ import Select from '../select'
 import SubmitButton from '../submit-button'
 const { Option } = Select
 
+// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 const Test = ({
   children,
   validate,
