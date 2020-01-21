ef69879f00044001abd2fda0e86b9ac65f1fd35b
Jannik Buschke <jannik.buschke@gmail.com>
2020-01-21 22:28:05 +0100

    format

--- src/field/index.tsx
+++ src/field/index.tsx
@@ -2,21 +2,16 @@ import { Field as FormikField, FastField } from 'formik'
 import { FormikFieldProps } from '../FieldProps'
 import * as React from 'react'
 
-export const Field: React.FC<FormikFieldProps> = ({ fastMode, children, ...restProps }) => {
+export const Field: React.FC<FormikFieldProps> = ({
+  fastMode,
+  children,
+  ...restProps
+}) => {
   if (fastMode) {
-    return (
-      <FastField {...restProps}>
-        {children}
-      </FastField>
-    )
+    return <FastField {...restProps}>{children}</FastField>
   }
 
-  return (
-    <FormikField {...restProps}>
-      {children}
-    </FormikField>
-  )
+  return <FormikField {...restProps}>{children}</FormikField>
 }
 
 export default Field
-
