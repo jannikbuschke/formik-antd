aeb539eeb9802d9e225c534f12ba9259ca9c814a
kzoeps <kzoepa@gmail.com>
2022-11-20 15:11:56 -0500

    test(select): add test for initial value and change of values

---
+++ src/select/index.test.tsx
@@ -0,0 +1,48 @@
+import { Formik } from "formik"
+import React from "react"
+import { Form } from '../form'
+import Select from './index'
+import '@testing-library/jest-dom/extend-expect'
+import { act } from 'react-dom/test-utils'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { SelectProps } from 'antd/lib/select'
+
+/*
+jest.mock('antd/lib/select', () = (props: SelectProps<any>) => {
+	return <select></select>
+})
+*/
+
+const TestSelect = () => {
+	return (
+		<Formik initialValues={{field: 0}} onSubmit={() => {}}>
+			<Form>
+				<Select name={'field'}>
+					<Select.Option value={0}>Zero</Select.Option>
+					<Select.Option value={1}>One</Select.Option>
+				</Select>
+			</Form>
+		</Formik>
+	)
+}
+
+test('renders select', async () => {
+	render(<TestSelect/>)
+	expect(screen.queryByRole('combobox')).toBeInTheDocument()
+})
+
+test('sets initial value', async () => {
+	const { queryByText } = render(<TestSelect/>)
+	expect(queryByText('Zero')).toBeInTheDocument()
+})
+
+test('changes selected upon clicking', async () => {
+	const { getByRole, queryByText, getByText, queryAllByText } = render(<TestSelect/>)
+	const selector = getByRole('combobox')
+	expect(queryByText('Zero')).toBeInTheDocument()
+	fireEvent.mouseDown(selector)
+	await act(async () => {
+		fireEvent.click(getByText('One'))
+	})
+	expect(queryAllByText('One').length).toBeGreaterThan(1)
+})
