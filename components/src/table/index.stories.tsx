a8dde46e6344375f62d9d879d8f275b2c4433003
Jannik <sisosys6@gmail.com>
2022-12-18 17:04:19 +0100

    improve table story and props

--- components/src/table/index.stories.tsx
+++ components/src/table/index.stories.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import { ComponentMeta } from '@storybook/react'
 import { StoryTemplate } from '../shared'
 import Component from './'
+import RemoveRowButton from '../remove-row-button'
+import AddRowButton from '../add-row-button'
+import Input from '../input'
 
 export default {
   title: 'Array/Table',
@@ -9,9 +12,49 @@ export default {
 } as ComponentMeta<typeof Component>
 
 export const Default = () => (
-  <StoryTemplate initialValues={{ value: true }}>
+  <StoryTemplate
+    initialValues={{
+      value: [
+        { id: '1', label: 'item 1' },
+        { id: '2', label: 'item 2' },
+        { id: '3', label: 'item 3' },
+      ],
+    }}
+  >
     <div>
-      <Component name='value' />
+      <Component<{ id: string; label: string }>
+        name='value'
+        pagination={false}
+        columns={[
+          {
+            title: 'Id',
+            key: 'id',
+            render: (_, record) => record.id,
+          },
+          {
+            title: 'Label',
+            key: 'label',
+            render: (_, record, i) => <Input name={`value.${i}.label`} />,
+          },
+          {
+            title: 'actions',
+            render: (_, record, i) => (
+              <RemoveRowButton name='value' index={i}>
+                Delete row
+              </RemoveRowButton>
+            ),
+          },
+        ]}
+      />
+      <AddRowButton
+        name='value'
+        createNewRow={() => {
+          const id = Math.random()
+          return { id, label: 'item ' + id }
+        }}
+      >
+        Add row
+      </AddRowButton>
     </div>
   </StoryTemplate>
 )
--- components/src/table/index.tsx
+++ components/src/table/index.tsx
@@ -1,17 +1,19 @@
 import { Field, FieldProps } from 'formik'
 import * as React from 'react'
 import { Table as $Table } from 'antd'
-import { TableProps } from 'antd/es/table/Table'
-// import { TableProps, WithStore } from 'antd/es/table';
+import { TableProps as $TableProps } from 'antd/es/table/Table'
+import { FormikFieldProps } from '../FieldProps'
 
 // RecordType extends object = any>(props: TableProps<RecordType>
 
 // declare function Table<RecordType extends object = any>(props: TableProps<RecordType>): JSX.Element;
 
+export type TableProps<RecordType> = FormikFieldProps & $TableProps<RecordType>
+
 export function Table<RecordType extends object = any>({
   name,
   ...restProps
-}: { name: string } & TableProps<RecordType>) {
+}: TableProps<RecordType>) {
   return (
     <Field name={name}>
       {({ field }: FieldProps<any>) => {
